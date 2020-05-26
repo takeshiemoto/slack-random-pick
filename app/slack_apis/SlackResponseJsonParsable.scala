414bd3a213a59c25caa9082fef5255dc4d01ca76
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-05-26 23:21:35 +0900

    Git init

---
+++ app/slack_apis/SlackResponseJsonParsable.scala
@@ -0,0 +1,31 @@
+package slack_apis
+
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json, Reads}
+import scala.util.{Success, Failure, Try}
+import slack_apis.errors.InvalidParamException
+
+trait SlackResponseJsonParsable {
+  def parse[A](responseJson: JsValue)(implicit reads: Reads[A]): Try[A] = {
+    val ok = (responseJson \ "OK").as[Boolean]
+    if (ok) {
+      Json.fromJson[A](responseJson) match {
+        case JsSuccess(result, _) =>
+          Success(result)
+        case _: JsError =>
+          Failure(new RuntimeException("Failure to parse JSON"))
+      }
+    } else {
+      Json.fromJson[SlackErrorResponse](responseJson)
+      match {
+        case JsSuccess(slackErrorResponse, _) =>
+          Failure(
+            InvalidParamException(slackErrorResponse.error)
+          )
+        case _: JsError =>
+          Failure(new RuntimeException(
+            "Slack API response is something wrong..."
+          ))
+      }
+    }
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := """slack_random_pick"""
+organization := "com.example"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.13.2"
+
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "com.example.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "com.example.binders._"
+
+libraryDependencies += ws
